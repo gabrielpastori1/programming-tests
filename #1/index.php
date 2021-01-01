559bbdb171a20a0f9e8ac5397d4d37ee7af0a61a
Gabriel Tedesco Pastori <gabrielpastori1@gmail.com>
2021-01-01 04:04:52 -0300

    Insue #1

---
+++ #1/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>#1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <img src="https://picsum.photos/id/684/600/400?blur=5" alt="">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+<?php
+$timeFinishBody = time();
+
+// Get the size of the output.
+$size = ob_get_length();
+
+header("Content-Length: {$size}");
+header("Connection: close");
+
+ob_end_flush();
+ob_flush();
+flush();
+//Running test
+sleep(15);
+$timeFileCreate = time();
+$dif = $timeFileCreate-$timeFinishBody;
+$content = "body: $timeFinishBody\nfile: $timeFileCreate\n\ndif: $dif";
+file_put_contents('log', $content);
